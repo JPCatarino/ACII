196f277c8f18519973f80a839e51c01fc0aefbc4
JPCatarino <jorge.catarino@ua.pt>
2018-06-06 00:51:03 +0100

    Complete UARTreview deleted compiler generated files

--- exteste/UARTreview.c
+++ exteste/UARTreview.c
@@ -1,10 +1,25 @@
+// Made by: JPCatarino  Date: 06/06/2018
+// Este programa foi feito com o objetivo de mostrar como fazer a configuração básica de uma UART para a DETPIC32
+// Contém também um pequeno exemplo de como ela pode ser usada, em que vamos transmitir o valor
+// dos DIPSWITCHES para os display cada vez que recebermos o carater S
 #include <detpic32.h>
+
+// Valor Global usado mais tarde para receber valores da UART
+char c;
+
+// Usado para configurar alguns dispositivos IO que irão ser usados para o teste da UART 
+void config_IO(){
+    // DISPLAYS and DIP
+    LATDbits.LATD5 = 1;
+    TRISB = (TRISB & 0x00FF) | 0x000F;
+    TRISD = TRISD & 0xFF9F;
+}
 
 void config_UART(){
     // 1º passo: configurar o gerador de baudrate de acordo com a taxa pretendida
     // BRGH (Divisão por 16 = 0 ; Divisão por 4 = 1)
     // BRG = (PBCLK/(BRGH * baudrate))-1
-    U1BRGbits.BRGH = 0;
+    U1MODEbits.BRGH = 0;
     U1BRG = 10;
     // 2º passo: declarar parametros da trama: 
     // dimensão da palavra a transmitir e tipo de paridade (PDSEL)
@@ -34,7 +49,7 @@ void config_interrupts(){
     // 2º passo: É necessário dar reset a flag e dar enable aos interrupts
     // temos 2 tipos de interrupts configuraveis: Transmissão (UxTXxx) ou Receção (UxRXxx)
     IFS0bits.U1RXIF = 0;
-    IES0bits.U1RXIE = 1;
+    IEC0bits.U1RXIE = 1;
 }
 
 // 2 funções normalmente usadas são a getc() para receber um carater e putc() para transmitir um carater
@@ -65,4 +80,29 @@ void putString(char *str)
         putc(str[i]);
         i++;
     }
+}
+
+void main(void){
+    config_UART();
+    config_interrupts();
+    EnableInterrupts();
+    config_IO();
+    static const char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
+    while(1){
+        getc();
+
+        if(c == 'S'){
+            int pval = PORTB & 0x000F;
+            LATB = ((int)display7Scodes[pval]<<8);
+        }
+    }
+
+}
+
+// interrupt handler para a UART1
+void _int_(24) isr_uart1(void){
+    // é ativada quando recebe um char
+    // esse char é atribuido então ao valor global c
+    c = U1RXREG;
+    IFS0bits.U1RXIF = 0;
 }
